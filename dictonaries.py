813374ece02bc1b9761369704065382f9b3ad7f9
denis007 <denis007@poczta.onet.eu>
2017-12-13 21:54:35 +0100

    Pierwsze podescie do @property https://www.programiz.com/python-programming/property

---
+++ dictonaries.py
@@ -0,0 +1,19 @@
+mydict = {'1':4,'b':55,'c':68}
+
+for key, value in mydict.items():
+    print("Key: "+str(key)+"Value: "+str(value))
+
+
+lista1=[(x,y) for x in range(1,6) for y in range(x,6)]
+key='b' in mydict
+print(key)
+if  key in mydict:
+    print("GG "+str(mydict[key]))
+
+
+print(list(lista1))
+
+listaaa= []
+
+if not listaaa:
+    print("Nie mam zawartości")
